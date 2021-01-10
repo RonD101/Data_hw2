cc38ace5b27056e88b6e42c86e0f4c930fcb151c
RonD101 <ron.dahan@campus.technion.ac.il>
2021-01-10 21:06:53 +0200

    DynamicArray.h Working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include "DynamicArray.h"
+
+
+using std::cout;
+using std::endl;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    DynamicArray<int> arr;
+    arr.push_back(2);
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(11);
+    arr.push_back(3);
+    arr.push_back(23);
+    for (int i = 0; i < arr.quantity(); ++i) {
+        cout << arr[i] <<endl;
+    }
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
     return 0;
 }
